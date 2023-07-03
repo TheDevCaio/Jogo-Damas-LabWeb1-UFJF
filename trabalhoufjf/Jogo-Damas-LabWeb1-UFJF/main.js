9a2c507237d3382c482ab69eed0a63d5aeb1ace6
Caio Dev <110746976+TheDevCaio@users.noreply.github.com>
2023-07-02 23:39:29 -0300

    Subindo arquivos.

---
+++ trabalhoufjf/Jogo-Damas-LabWeb1-UFJF/main.js
@@ -0,0 +1,68 @@
+import { getTabuleiro } from "./state.js";
+
+const eTabuleiro = document.querySelector(".tabuleiro");
+//const disco = document.querySelector(".disco");
+
+
+//disco.addEventListener('dragstart', comecaArrastar);
+
+
+let arrastado = null;
+setup();
+
+function comecaArrastar(evento){
+    arrastado = evento.target;
+}
+
+function recebeAlgo(evento){
+    if(arrastado){
+        if (evento.target.classList.contains('casa')) {
+            evento.target.appendChild(arrastado);
+            const posDisco = arrastado.dataset.posicao;
+            const posCasa = evento.target.dataset.posicao;
+            console.log(`moveu disco de ${posDisco} para ${posCasa}`);
+            arrastado = null;
+        }
+    }
+}
+
+function passouPorCima(evento){
+    evento.preventDefault();
+}
+
+function setup(){
+    const tabuleiro = getTabuleiro();
+    for (let i = 0; i < tabuleiro.length; i++) {
+        const casa = tabuleiro[i];
+        const eCasa = criaCasa(casa, i);
+        eTabuleiro.appendChild(eCasa);
+    }
+}
+
+function criaCasa(casa, k){
+    const eCasa = document.createElement('div');
+    eCasa.dataset.posicao = k;
+    eCasa.classList.add("casa");
+    eCasa.addEventListener('dragover', passouPorCima);
+    eCasa.addEventListener('drop', recebeAlgo);
+
+    if (casa){
+        const eDisco = criaDisco(casa, k);
+        eCasa.appendChild(eDisco);
+    }
+    return eCasa;
+}
+
+function criaDisco(casa, k) {
+    const eDisco = document.createElement('div');
+    eDisco.draggable = true;
+    eDisco.dataset.posicao = k;
+    eDisco.classList.add('disco');
+    eDisco.addEventListener('dragstart', comecaArrastar);
+    if(casa == 'p'){
+        eDisco.classList.add('preto');
+    }else{
+        eDisco.classList.add('branco');
+    }
+    return eDisco;
+}
